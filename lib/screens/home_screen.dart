9db2a629be74b52386b75c5e895c4c5516db73a9
Zahid Hasan Tonmoy <zahidhasantonmoy360@gmail.com>
2025-08-15 00:14:27 +0600

    feat: add home screen

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,27 @@
+
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.logout),
+            onPressed: () {
+              FirebaseAuth.instance.signOut();
+            },
+          ),
+        ],
+      ),
+      body: const Center(
+        child: Text('Welcome!'),
+      ),
+    );
+  }
+}
