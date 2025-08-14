841fa3bc8e34d01f74dca3038d7c2c656f75e54c
Zahid Hasan Tonmoy <zahidhasantonmoy360@gmail.com>
2025-08-15 01:26:18 +0600

    fix: compilation error in animated_background.dart

--- lib/widgets/animated_background.dart
+++ lib/widgets/animated_background.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 import 'package:simple_animations/simple_animations.dart';
 
@@ -6,14 +7,18 @@ class AnimatedBackground extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return PlayAnimation<Color?>(
-      tween: ColorTween(begin: Colors.deepPurple.shade400, end: Colors.purple.shade400),
-      duration: const Duration(seconds: 3),
+    final tween = MovieTween()
+      ..tween('color1', ColorTween(begin: Colors.deepPurple.shade400, end: Colors.purple.shade400), duration: const Duration(seconds: 3))
+      ..tween('color2', ColorTween(begin: Colors.deepPurple.shade800, end: Colors.purple.shade800), duration: const Duration(seconds: 3));
+
+    return LoopAnimation<Movie>(
+      tween: tween,
+      duration: tween.duration,
       builder: (context, child, value) {
         return Container(
           decoration: BoxDecoration(
             gradient: LinearGradient(
-              colors: [value ?? Colors.deepPurple.shade400, Colors.deepPurple.shade800],
+              colors: [value.get('color1'), value.get('color2')],
               begin: Alignment.topLeft,
               end: Alignment.bottomRight,
             ),
