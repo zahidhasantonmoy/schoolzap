f55fe48ec6c5490839a2b1a2b922e3f86eff5259
Zahid Hasan Tonmoy <zahidhasantonmoy360@gmail.com>
2025-08-15 22:46:42 +0600

    feat: Create AcademicYear model

---
+++ lib/models/academic_year.dart
@@ -0,0 +1,13 @@
+class AcademicYear {
+  final String id;
+  final String name;
+  final DateTime startDate;
+  final DateTime endDate;
+
+  AcademicYear({
+    required this.id,
+    required this.name,
+    required this.startDate,
+    required this.endDate,
+  });
+}
