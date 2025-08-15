baa7fd749b642bc36b7327a880e2b46f843a6706
Zahid Hasan Tonmoy <zahidhasantonmoy360@gmail.com>
2025-08-15 22:18:36 +0600

    feat: Create School model

---
+++ lib/models/school.dart
@@ -0,0 +1,13 @@
+class School {
+  final String id;
+  final String name;
+  final String adminId;
+  final String subscriptionPlanId;
+
+  School({
+    required this.id,
+    required this.name,
+    required this.adminId,
+    required this.subscriptionPlanId,
+  });
+}
