e5d49796801a3204f009e32c0dc356999cefc5dd
Zahid Hasan Tonmoy <zahidhasantonmoy360@gmail.com>
2025-08-15 22:18:53 +0600

    feat: Create SubscriptionPlan model

---
+++ lib/models/subscription_plan.dart
@@ -0,0 +1,11 @@
+class SubscriptionPlan {
+  final String id;
+  final String name;
+  final List<String> features;
+
+  SubscriptionPlan({
+    required this.id,
+    required this.name,
+    required this.features,
+  });
+}
