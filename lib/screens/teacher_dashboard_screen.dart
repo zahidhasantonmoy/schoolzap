f4f5cdc1edf8bf0865c61fca30d50268415a7831
Zahid Hasan Tonmoy <zahidhasantonmoy360@gmail.com>
2025-08-15 22:53:07 +0600

    feat: Create TeacherDashboardScreen

---
+++ lib/screens/teacher_dashboard_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class TeacherDashboardScreen extends StatelessWidget {
+  const TeacherDashboardScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Teacher Dashboard'),
+      ),
+      body: const Center(
+        child: Text('Teacher Dashboard Screen'),
+      ),
+    );
+  }
+}
