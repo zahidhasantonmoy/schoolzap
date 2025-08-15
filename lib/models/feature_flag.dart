6381e0bbc49b7c90d627778085fc1c18d821c352
Zahid Hasan Tonmoy <zahidhasantonmoy360@gmail.com>
2025-08-15 22:19:18 +0600

    feat: Create FeatureFlag model

---
+++ lib/models/feature_flag.dart
@@ -0,0 +1,11 @@
+class FeatureFlag {
+  final String id;
+  final String name;
+  final bool isEnabled;
+
+  FeatureFlag({
+    required this.id,
+    required this.name,
+    required this.isEnabled,
+  });
+}
