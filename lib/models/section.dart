c272166cb1aba461aa5ba9d3a869b950501374dd
Zahid Hasan Tonmoy <zahidhasantonmoy360@gmail.com>
2025-08-15 22:47:27 +0600

    feat: Create Section model

---
+++ lib/models/section.dart
@@ -0,0 +1,11 @@
+class Section {
+  final String id;
+  final String name;
+  final String classId;
+
+  Section({
+    required this.id,
+    required this.name,
+    required this.classId,
+  });
+}
