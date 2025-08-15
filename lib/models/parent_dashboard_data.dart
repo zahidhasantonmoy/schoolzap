c3ca612faf5bd93a27e2c3869f86997d8e55a684
Zahid Hasan Tonmoy <zahidhasantonmoy360@gmail.com>
2025-08-15 23:04:23 +0600

    feat: Create ParentDashboardData model

---
+++ lib/models/parent_dashboard_data.dart
@@ -0,0 +1,9 @@
+class ParentDashboardData {
+  final List<String> childrenNames;
+  final List<String> recentUpdates;
+
+  ParentDashboardData({
+    required this.childrenNames,
+    required this.recentUpdates,
+  });
+}
