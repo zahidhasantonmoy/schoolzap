c894d756ea8a628722c7334904d4df249ac58914
Zahid Hasan Tonmoy <zahidhasantonmoy360@gmail.com>
2025-08-15 22:52:48 +0600

    feat: Create Grade model

---
+++ lib/models/grade.dart
@@ -0,0 +1,13 @@
+class Grade {
+  final String id;
+  final String submissionId;
+  final double score;
+  final String feedback;
+
+  Grade({
+    required this.id,
+    required this.submissionId,
+    required this.score,
+    required this.feedback,
+  });
+}
