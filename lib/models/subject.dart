8f72372bd671d8ea96158d53913f1100e7d70f6f
Zahid Hasan Tonmoy <zahidhasantonmoy360@gmail.com>
2025-08-15 22:47:50 +0600

    feat: Create Subject model

---
+++ lib/models/subject.dart
@@ -0,0 +1,11 @@
+class Subject {
+  final String id;
+  final String name;
+  final String classId;
+
+  Subject({
+    required this.id,
+    required this.name,
+    required this.classId,
+  });
+}
