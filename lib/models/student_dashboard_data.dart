464240d7ac7b20394f9fe1427db8e1ff79f369cd
Zahid Hasan Tonmoy <zahidhasantonmoy360@gmail.com>
2025-08-15 23:00:06 +0600

    feat: Create StudentDashboardData model

---
+++ lib/models/student_dashboard_data.dart
@@ -0,0 +1,9 @@
+class StudentDashboardData {
+  final List<String> upcomingAssignments;
+  final List<String> upcomingEvents;
+
+  StudentDashboardData({
+    required this.upcomingAssignments,
+    required this.upcomingEvents,
+  });
+}
