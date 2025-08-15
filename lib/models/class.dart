5178fed6af576f429b6c06abe056a71aa13521e0
Zahid Hasan Tonmoy <zahidhasantonmoy360@gmail.com>
2025-08-15 22:47:05 +0600

    feat: Create Class model

---
+++ lib/models/class.dart
@@ -0,0 +1,11 @@
+class Class {
+  final String id;
+  final String name;
+  final String academicYearId;
+
+  Class({
+    required this.id,
+    required this.name,
+    required this.academicYearId,
+  });
+}
