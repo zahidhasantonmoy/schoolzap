77f214791893cdc76520e0c85931ae56fa71baea
Zahid Hasan Tonmoy <zahidhasantonmoy360@gmail.com>
2025-08-15 22:52:12 +0600

    feat: Create Assignment model

---
+++ lib/models/assignment.dart
@@ -0,0 +1,17 @@
+class Assignment {
+  final String id;
+  final String title;
+  final String description;
+  final DateTime dueDate;
+  final String classId;
+  final String subjectId;
+
+  Assignment({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.dueDate,
+    required this.classId,
+    required this.subjectId,
+  });
+}
