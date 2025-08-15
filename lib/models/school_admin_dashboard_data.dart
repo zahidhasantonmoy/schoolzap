f20124dc96aba7f9c3ce8ba8613e5f7b412b8925
Zahid Hasan Tonmoy <zahidhasantonmoy360@gmail.com>
2025-08-15 22:40:50 +0600

    feat: Create SchoolAdminDashboardData model

---
+++ lib/models/school_admin_dashboard_data.dart
@@ -0,0 +1,13 @@
+class SchoolAdminDashboardData {
+  final int totalStudents;
+  final int totalTeachers;
+  final int pendingFees;
+  final int upcomingEvents;
+
+  SchoolAdminDashboardData({
+    required this.totalStudents,
+    required this.totalTeachers,
+    required this.pendingFees,
+    required this.upcomingEvents,
+  });
+}
